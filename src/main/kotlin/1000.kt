55f79649ef67f5e5a78df0dda6c9a2273450bf9b
jeondoohyun <sonlcr1@naver.com>
2023-07-11 19:54:01 +0900

    1181 clear

--- src/main/kotlin/1000.kt
+++ src/main/kotlin/1000.kt
@@ -7,7 +7,7 @@ import java.util.*
 // kotlin 클래스 파일이 아니라 kotlin file로 만들어야함
 
 // 1000
-fun main(args: Array<String>) = with(Scanner(System.`in`)){ // with을 사용하면 scan.nextInt()의 인스턴스 없이 바로 nextInt()만 사용가능
+fun main(args: Array<String>) = with(Scanner(System.`in`)){ // with을 사용하면 scan.nextInt()의 형태가 아닌 바로 nextInt()만 사용가능
     println(nextInt()+nextInt())
 }
 
