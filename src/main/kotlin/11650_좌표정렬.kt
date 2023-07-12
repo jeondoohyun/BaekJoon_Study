5b8aecba2dd697a93dbdae3e5b3189c5c3dc6bb2
dhjeon <sonlcr1@naver.com>
2023-07-13 02:02:57 +0900

    11650 ing

--- src/main/kotlin/11650_좌표정렬.kt
+++ src/main/kotlin/11650_좌표정렬.kt
@@ -1,3 +1,19 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+data class XY (val x: Int, val y: Int)
 fun main() = with(System.out.bufferedWriter()) {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    var N = br.readLine().toInt()
+    var XY_List = mutableListOf<XY>()
+
+    for (i in 0 until N) {
+        var coor_ = br.readLine().split(" ")
+        XY_List.add(XY(coor_[0].toInt(), coor_[1].toInt()))
+//        println("${coor_[0].toInt()}, ${coor_[1].toInt()}")
+    }
+    // todo : x,y 좌표 각각 비교해서 정렬
+
 
 }
