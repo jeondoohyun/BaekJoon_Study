7200b0fd5aa09e26b3a0e827dc44fe73edf48d31
dhjeon <sonlcr1@naver.com>
2023-07-21 01:18:31 +0900

    1202 ing

--- src/main/kotlin/1202_보석도둑.kt
+++ src/main/kotlin/1202_보석도둑.kt
@@ -2,6 +2,7 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import java.util.PriorityQueue
 
 data class Jewelry(var M: Int, var V: Int)
 
@@ -10,23 +11,16 @@ fun main(args: Array<String>) {
     var br = BufferedReader(InputStreamReader(System.`in`))
     var bw = BufferedWriter(OutputStreamWriter(System.out))
 
-    var arr = mutableListOf<Jewelry>()
+    var queue = PriorityQueue<Jewelry>()
 
     var N_K = br.readLine().split(" ").map { it.toInt() }
 
     for (i in 0 until N_K[0]) {
         var n_k = br.readLine().split(" ").map { it.toInt() }
-        arr.add(Jewelry(n_k[0], n_k[1]))
+        queue.add(Jewelry(n_k[0], n_k[1]))
     }
 
-    var C = mutableListOf<Int>()
-    for (i in 0 until N_K[1]) {
-        C.add(br.readLine().toInt())
-    }
-
-    C.sort()
-
-    var arr_2 = arr.sortedWith(Comparator { t, t2 ->
+    var queue2 = queue.sortedWith(Comparator { t, t2 ->
         when {
             t.V < t2.V -> 1
             t.V == t2.V -> when {
@@ -36,25 +30,45 @@ fun main(args: Array<String>) {
             else -> -1
         }
     })
+    // todo : 우선순위큐로 가격기준으로 내림차순 정렬이 안됨
+
+    print("${queue2.toString()}")
+
+    var C = mutableListOf<Int>()
+    for (i in 0 until N_K[1]) {
+        C.add(br.readLine().toInt())
+    }
+
+    C.sort()
+
+//    var arr_2 = arr.sortedWith(Comparator { t, t2 ->
+//        when {
+//            t.V < t2.V -> 1
+//            t.V == t2.V -> when {
+//                t.M < t2.M -> -1
+//                else -> 1
+//            }
+//            else -> -1
+//        }
+//    })
 
     var result = 0
     var num = 0
 
-    C.forEach {a ->
-        run {
-            arr_2.forEach {b ->
-                if (a >= b.M) {
-                    result += b.V
-                    return@run
-                }
-            }
-        }
+//    C.forEach {a ->
+//        run {
+//            arr_2.forEach {b ->
+//                if (a >= b.M) {
+//                    result += b.V
+//                    return@run
+//                }
+//            }
+//        }
+//    }
 
-    }
-
-    bw.write("$result")
-    bw.flush()
-    bw.close()
+//    bw.write("$result")
+//    bw.flush()
+//    bw.close()
 
 
 }
