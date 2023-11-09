4239fbd3a06aa6a269ba830ca5fa7d3e0a7a01a2
dhjeon <sonlcr1@naver.com>
2023-11-09 20:22:38 +0900

    12891 보완완료

--- src/main/kotlin/12891_dna비밀번호.kt
+++ src/main/kotlin/12891_dna비밀번호.kt
@@ -1,7 +1,9 @@
 import java.io.*
 
-// 슬라이딩 윈도우
+// 슬라이딩 윈도우 기법을 사용
+
 var conditionArr = IntArray(4)
+var checkCnt = 0    // checkCnt가 4가 되면 (각자리 조건4개가 충족되면) result +1
 fun main(args: Array<String>) {
 
     var br = BufferedReader(InputStreamReader(System.`in`))
@@ -24,21 +26,23 @@ fun main(args: Array<String>) {
     conditionArr = IntArray(4){0}
     for (i in c.indices) {  // 0 until c.size
         conditionArr[i] = c[i]
+        if (c[i] == 0) checkCnt++   // 0개라는건 없어도 된다는거니까 무조건 조건충족이라 checkCnt를 +1
     }
 
+    // 부분문자열 길이 만큼의 dna배열 만들기
     for (i in 0 until p) {
-        dnaArr = addDna(dna, i, dnaArr)
+        dnaArr = addDna(dna[i], dnaArr)
     }
 
-    result += compareCondition(dnaArr)
+    if (checkCnt == 4) result++
 
     // n : 슬라이딩 하는 횟수
     var n = s-p
     var tmpDnaArr = dnaArr
     for (i in 0 until  n) {
-        tmpDnaArr = removeDna(dna, i, tmpDnaArr)
-        tmpDnaArr = addDna(dna, p+i, tmpDnaArr)
-        result += compareCondition(tmpDnaArr)
+        tmpDnaArr = removeDna(dna[i], tmpDnaArr)
+        tmpDnaArr = addDna(dna[p+i], tmpDnaArr)
+        if (checkCnt == 4) result++
     }
 
     bw.write(result.toString())
@@ -46,33 +50,39 @@ fun main(args: Array<String>) {
     bw.close()
 }
 
-fun compareCondition(dnaArr: IntArray): Int {
-    var a = 0
-    for (i in conditionArr.indices) {
-        if (conditionArr[i] > dnaArr[i]) break
-        else if(i == 3) a++
+fun addDna(s: Char, dnaArr: IntArray): IntArray {
+    var tmpDnaArr = dnaArr
+    if (s == 'A') {
+        tmpDnaArr[0]++
+        if (tmpDnaArr[0] == conditionArr[0]) checkCnt++     // 충족된 순간 한번만 +1해준다. 조건문을 >= 이렇게 해놓으면 한자리에 checkCnt가 여러번 증가 될수 있다.
+    } else if (s == 'C') {
+        tmpDnaArr[1]++
+        if (tmpDnaArr[1] == conditionArr[1]) checkCnt++
+    } else if (s == 'G') {
+        tmpDnaArr[2]++
+        if (tmpDnaArr[2] == conditionArr[2]) checkCnt++
+    } else if (s == 'T') {
+        tmpDnaArr[3]++
+        if (tmpDnaArr[3] == conditionArr[3]) checkCnt++
     }
-    return a
-}
-
-fun removeDna(dna: String, i: Int, dnaArr: IntArray): IntArray {
-    var s = dna[i]
-    var tmpDnaArr = dnaArr
-    if (s == 'A') tmpDnaArr[0]--
-    else if (s == 'C') tmpDnaArr[1]--
-    else if (s == 'G') tmpDnaArr[2]--
-    else if (s == 'T') tmpDnaArr[3]--
 
     return tmpDnaArr
 }
-
-fun addDna(dna: String, i: Int, dnaArr: IntArray): IntArray {
-    var s = dna[i]
+fun removeDna(s: Char, dnaArr: IntArray): IntArray {
     var tmpDnaArr = dnaArr
-    if (s == 'A') tmpDnaArr[0]++
-    else if (s == 'C') tmpDnaArr[1]++
-    else if (s == 'G') tmpDnaArr[2]++
-    else if (s == 'T') tmpDnaArr[3]++
-
+    if (s == 'A') {
+        if (tmpDnaArr[0] == conditionArr[0]) checkCnt--     // 지금은 같지만 다음 라인에서 -- 되기 때문에 충족값보다 적어지기 때문에 checkCnt를 -1 해준다.
+        tmpDnaArr[0]--
+    } else if (s == 'C') {
+        if (tmpDnaArr[1] == conditionArr[1]) checkCnt--
+        tmpDnaArr[1]--
+    } else if (s == 'G') {
+        if (tmpDnaArr[2] == conditionArr[2]) checkCnt--
+        tmpDnaArr[2]--
+    } else if (s == 'T') {
+        if (tmpDnaArr[3] == conditionArr[3]) checkCnt--
+        tmpDnaArr[3]--
+    }
     return tmpDnaArr
 }
+
